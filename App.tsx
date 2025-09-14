702e36851fb48105afcec9f1cd8a36889cc76082
Tauã Mendonça <63937691+tauamendonca@users.noreply.github.com>
2025-09-14 08:42:50 -0300

    First commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from "react-native";
+import { StyleSheet } from 'react-native';
+import Home from "./src/screens/home";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style = {styles.start}>
+      <Home />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  start: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
